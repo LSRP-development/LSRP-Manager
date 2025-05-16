56de98a01167b28f696695c6eee345d7b5341759
Filipo <filgreen@pm.me>
2025-05-16 22:41:55 +0200

    Fix config

--- src/config/a_main_config.ts
+++ src/config/a_main_config.ts
@@ -4,7 +4,7 @@ const config: IConfig = {
   mainGuildID: "1061028022045651014",
   devUserIDs: ["705701074975195178"],
   devGuildID: "1189318241831616562",
-  shrRole: "1264985065864302852",
+  shrRole: "1240781497443815560",
   emoji: {
     loading: "<a:Loading:1358001524747272254>"
   },
